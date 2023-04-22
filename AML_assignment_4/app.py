2e0f34536114fba8d5eaeb4547dea3835cda6feb
Naheli98 <112321792+Naheli98@users.noreply.github.com>
2023-04-22 23:09:35 +0530

    Add files via upload

---
+++ AML_assignment_4/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+import joblib
+from score import score
+
+app = Flask(__name__)
+
+model = joblib.load('model.joblib')
+
+@app.route('/')
+def score_text():
+    # Get the text input from the request body
+    text = 'This is a test'
+    
+    # Use the score function to get the prediction and propensity
+    prediction, propensity = score(text, model, threshold=0.5)
+    
+    # Create a response dictionary with the prediction and propensity
+    response = {'prediction': prediction, 'propensity': propensity}
+    
+    # Return the response in JSON format
+    return jsonify(response)
+ 
+# main driver function
+if __name__ == '__main__':
+ 
+    # run() method of Flask class runs the application
+    # on the local development server.
+    app.run(host='0.0.0.0', port=8000)
